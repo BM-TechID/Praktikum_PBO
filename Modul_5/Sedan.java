e7b26268d0bd54754ef256e49ff3ccd1ca520a66
BM-TechID <50511924+BM-TechID@users.noreply.github.com>
2023-05-21 13:22:34 +0700

    Add files via upload

---
+++ Modul_5/Sedan.java
@@ -0,0 +1,26 @@
+public class Sedan extends Mobil {
+    private String fasilitasKeamanan;
+    private int kapasitasCC;
+    private String fasilitasKenyamanan;
+
+    public Sedan(String noPlat, String merk, double pajak, String fasilitasKeamanan, int kapasitasCC, String fasilitasKenyamanan) {
+        super(noPlat, merk, pajak);
+        this.fasilitasKeamanan = fasilitasKeamanan;
+        this.kapasitasCC = kapasitasCC;
+        this.fasilitasKenyamanan = fasilitasKenyamanan;
+    }
+
+    public void infoSedan() {
+        System.out.println("Fasilitas Keamanan: " + fasilitasKeamanan);
+        System.out.println("Kapasitas CC: " + kapasitasCC);
+        System.out.println("Fasilitas Kenyamanan: " + fasilitasKenyamanan);
+    }
+
+    public void tampilInfo() {
+        super.tampilInfo();
+    }
+
+    public float hitungPajak() {
+        return (float) (pajak + (pajak * kapasitasCC * 0.00005));
+    }
+}
